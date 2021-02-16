b70bbade3dd48045d550f7c589fdfdf799969256
PavlinaBaldova <pavlina.bal@seznam.cz>
2021-02-16 09:42:04 +0100

    fce tasks

--- index.js
+++ index.js
@@ -1,106 +1,15 @@
+"use strict";
 console.log("it works");
 
-/* Úloha 1 - Převod měny
+/*Napište funkci makeBorder, která jako vstup obdrží DOM element. Funkce nebude nic vracet, pouze předanému elementu nastaví černý rámeček tlustý 2 pixely. Vložte na stránku nějaký obsah a v konzoli vyzkoušejte, že vaše funkce funguje.*/
 
-Dejme tomu, že si jako programátoři vyděláváte 20 euro na hodinu. Uložte tuto hodnotu do proměnné wageInEur.
-Spočítejte, kolik je vaše hodinová mzda v českých korunách, jestliže kurz eura je 26.58 kč. Výsledek zaokrouhlete na celé koruny a uložte do proměnné wageInCzk.
-Vypište obsah proměnné wageInCzk do webové stránky tak, aby na stránce byl nadpis h1 s obsahem:
-Mzda v korunách: 532 kč*/
+const makeBorder = () => {
+  const makeBorderElm = document.querySelector("h1");
+  makeBorderElm.style.border = "1px solid black"
+  };
 
-const wageInEur = 20;
-const wageInCzk = Math.round(20 * 26.58);
-
-document.write(`<h1>Vaše hodinová mzda je ${wageInCzk}</h1>`);
-
-
-
-
-
-
-/* Úloha 2 - Knihovna
-
-V JavaScriptovém programu vytvořte objekt představující jednu knihu v knihovně. Uvažte, jaké vlastnosti může taková kniha. 
-Rozhodně budeme chtít název, autora a počet stran. Přidejte do objektu alespoň tři další vlastnosti tak, 
-aby obsahovaly čísla, řetězce i vnořený objekt.
-Vytvořte alespoň jednu další knihu se stejnými vlastnostmi ale jinými hodnotami.*/
-
-
-const books = {
-  author: "George Orwell",
-  name: "1984",
-  pages: 220,
-  publish: {
-    publisher: "Nakladatelství",
-    year: 2010,
-  },
-}
-
-  const { author, name, pages } = books;
-
-  const content = `Kniha má ${pages} stránek a napsal ji ${author}`;
-  
-  document.write(content);
-
-
-
- /* author: "Erich Maria Remarque",
-  name: "Na západní frontě klid",
-  pages: 250,
-  publish: {
-    publisher: "Epocha",
-    year: 2008,
-  }
-}*/
-
-/*může být tak, nebo lepší mít druhou knihu jako proměnnou book2. Jak se dostanu přes tečkovou anotaci
-k první knize? Když chci vypsat obě knihy, dám je do pole a vymapuji?*/
-
-
-
-
-
-
-
-
-
-/*Úloha 3 - Registrace na očkování
-
-V předchozí lekci jsem vytvářeli stránku pro registraci na očkování. Chtěli jsme po uživateli jméno a věk. 
-Nyní budeme chtít, aby uživatel zadal také heslo. To bychom v pozdější verzi aplikaci mohli použít například
- k přihlášení do systému.
-
-Jakmile uživatel zadá věk, zkontrolujte, že je větší nebo roven 65. Pokud ano, vypište věk v pořádku.
-Pokud uživatel nemá alespoň 65 let, vypište Nízký věk.
-Jakmile uživatel zadá heslo, zkontrolujte, že je delší než osm znaků. Pokud není, vypište slabé heslo.*/
-
-const age = Number(prompt("Zadejte věk"));
-const password = String(prompt("Zadejte heslo"))
-
-if (age >= 65) {
-document.write("v pořádku");
-}
-else {
-  document.write("nízký věk");
-}
-
-if (password.length < 8) {
-  document.write("slabé heslo");
-}
-
-/*zkusit více podmínek na zadání hesla*/
-
-
-
-/* Úloha 4 -
-Na obrázku vidíte rozložení čísel na klasické Francouzské ruletě. Ruleta obsahuje čísla 0 - 36. 
-Každé číslo s výjimkou nuly je buď sudé nebo liché a zároveň červené nebo černé. Pro čísla 1 až 10 a 19 až 28 platí, že 
-lichá čísla jsou červená a sudá jsou černá. Pro zbytek platí obrácené pravidlo, tedy lichá jsou černá a sudá červená. 
-Nula není ani lichá ani sudá, ani černá ani červená.
-
-
-
-Vytvořte stránku, které uživatel zadá číslo a stránka odpoví jestli jde o číslo sudé nebo liché, 
-černé nebo červené, nebo je to nula.*/
+/*otázky fce:
+jaký je rozdíl mezi vstupem a použitou proměnnou*/
 
 
 
@@ -112,20 +21,15 @@ Vytvořte stránku, které uživatel zadá číslo a stránka odpoví jestli jde
 
 
 
+  /*otázky:
+  používají se v praxi :nth-child, last-of-type
+  máš zkušenosti s .Netem*/
 
 
 
-/*dotaz*/
 
-let sazba = 500;
-let vyplata = 8 * 21 * sazba;
-console.log(vyplata);
 
-sazba = 700;
-vyplata = 8 * 21 * sazba
-console.log(vyplata);
 
-/*může se takto psát let proměnná?, ale ne u const*/
 
 
 
